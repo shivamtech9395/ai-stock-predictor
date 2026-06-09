fb4af63af748b129ad771c4c3d3d8437a486ceb6
shivamtech9395 <shivam2028.sv@gmail.com>
2026-06-09 16:16:52 +0530

    Add Market Observation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Dashboard from './pages/Dashboard'
 import News from './pages/News'
 import StockDetail from './pages/StockDetail'
 import MarketObservation from './pages/MarketObservation'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/news" element={<News />} />
-        <Route path="/stock/:sy++++++++++++mbol" element={<StockDetail />} />
+        <Route path="/stock/:symbol" element={<StockDetail />} />
         <Route path="/market" element={<MarketObservation />} />
       </Routes>
     </BrowserRouter>
